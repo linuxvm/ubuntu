6f46a2890fe88e993bd6d6adad1d3402235607e3
JavaCommons <javacommons@gmail.com>
2017-03-26 03:11:53 +0900

    .

--- 16.04/server/setup2.sh
+++ 16.04/server/setup2.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 LANG=C
+# tasksel ==> ubuntu gnome desktop
 sudo apt-get update
+sudo apt-get -y install build-essential
 sudo apt-get -y install gedit
 sudo apt-get -y install software-center
-sudo apt-get -y install build-essential
 sudo apt-get -y install language-pack-ja-base language-pack-ja
-# tasksel ==> ubuntu gnome desktop
+sudo apt-get -y install ibus-mozc
+sudo apt-get -y install firefox-locale-ja
