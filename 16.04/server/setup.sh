64cdcb96b6f4a004e322ae17b557c8fe1a3a211b
JavaCommons <javacommons@gmail.com>
2017-03-26 08:56:28 +0900

    .

--- 16.04/server/setup.sh
+++ 16.04/server/setup.sh
@@ -7,8 +7,4 @@ sudo apt-get -y install software-center
 sudo apt-get -y install build-essential
 sudo apt-get -y install language-pack-ja language-pack-gnome-ja fonts-takao ibus-anthy ibus-mozc fcitx-mozc
 sudo apt-get -y install firefox-locale-ja
-
-#sudo apt-get -y install unity-tweak-tool
-#gsettings set org.gnome.desktop.interface monospace-font-name 'Ubuntu Mono 13'
-#gsettings set org.gnome.desktop.interface font-name 'Ubuntu 11'
-#gsettings set org.gnome.desktop.interface document-font-name 'Sans 11'
+setxkbmap -rules evdev -model jp106 -layout jp
