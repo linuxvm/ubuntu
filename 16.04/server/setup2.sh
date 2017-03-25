d91c00da7101d92b3c64961b26f0d03c432e6002
JavaCommons <javacommons@gmail.com>
2017-03-26 02:38:36 +0900

    .

--- 16.04/server/setup2.sh
+++ 16.04/server/setup2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 LANG=C
 sudo apt-get update
-sudo apt-get -y install gnome-desktop
 sudo apt-get -y install gedit
 sudo apt-get -y install software-center
 sudo apt-get -y install build-essential
 sudo apt-get -y install language-pack-ja-base language-pack-ja
+# tasksel ==> ubuntu gnome desktop
