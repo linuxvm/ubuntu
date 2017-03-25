85c714147f4f127b912e749619ef6af668b0bf48
JavaCommons <javacommons@gmail.com>
2017-03-26 02:27:05 +0900

    .

---
+++ 16.04/server/setup2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get -y install gnome-desktop
+sudo apt-get -y install gedit
+sudo apt-get -y install software-center
+sudo apt-get -y install build-essential
+sudo apt-get -y install language-pack-ja-base language-pack-ja
