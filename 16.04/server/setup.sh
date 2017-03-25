6865392acbe3d038e941e1228db83165c3932cfb
JavaCommons <javacommons@gmail.com>
2017-03-25 23:36:01 +0900

    .

--- 16.04/server/setup.sh
+++ 16.04/server/setup.sh
@@ -2,4 +2,5 @@
 sudo apt-get update
 sudo apt-get -y install --no-install-recommends ubuntu-desktop
 sudo apt-get -y install gnome-terminal
+sudo apt-get -y install nautilus
 sudo apt-get -y install gedit
