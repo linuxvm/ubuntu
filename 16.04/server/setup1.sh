33a7cb6b429a0ed1fdf0f80b40ef997d68310211
JavaCommons <javacommons@gmail.com>
2017-03-26 13:28:51 +0900

    .

--- 16.04/server/setup1.sh
+++ 16.04/server/setup1.sh
@@ -7,3 +7,5 @@ sudo apt-get -y install gedit
 sudo apt-get -y install software-center
 sudo apt-get -y install language-pack-ja language-pack-gnome-ja fonts-takao ibus-anthy ibus-mozc
 sudo apt-get -y install firefox-locale-ja
+sudo timedatectl set-timezone Asia/Tokyo
+sudo timedatectl
