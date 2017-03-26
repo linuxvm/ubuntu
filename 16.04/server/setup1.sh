727a2d4e337510a93e7ec53b0682e1d715751215
JavaCommons <javacommons@gmail.com>
2017-03-26 12:40:57 +0900

    .

--- 16.04/server/setup1.sh
+++ 16.04/server/setup1.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 LANG=C
 sudo apt-get update
+sudo apt-get -y install build-essential
 sudo apt-get -y install gnome-core
 sudo apt-get -y install gedit
 sudo apt-get -y install software-center
-sudo apt-get -y install build-essential
-#sudo apt-get -y install language-pack-ja language-pack-gnome-ja fonts-takao ibus-anthy ibus-mozc fcitx-mozc
 sudo apt-get -y install language-pack-ja language-pack-gnome-ja fonts-takao ibus-anthy ibus-mozc
 sudo apt-get -y install firefox-locale-ja
-#setxkbmap -rules evdev -model jp106 -layout jp
