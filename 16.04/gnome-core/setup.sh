7e1c8ceb342a2abb66a1d81e081e8553895f63a4
JavaCommons <javacommons@gmail.com>
2017-03-27 20:14:06 +0900

    .

--- 16.04/gnome-core/setup.sh
+++ 16.04/gnome-core/setup.sh
@@ -6,6 +6,5 @@ sudo apt-get -y install gnome-core
 sudo apt-get -y install gedit
 sudo apt-get -y install software-center
 sudo apt-get -y install language-pack-ja language-pack-gnome-ja fonts-takao ibus-mozc
+sudo apt-get -y install fcitx fcitx-ui-classic fcitx-frontend-gtk2 fcitx-frontend-gtk3 fcitx-frontend-qt4 fcitx-mozc
 sudo apt-get -y install firefox-locale-ja
-sudo timedatectl set-timezone Asia/Tokyo
-sudo timedatectl
--- 16.04/gnome/setup2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-LANG=C
-setxkbmap -rules evdev -model jp106 -layout jp
